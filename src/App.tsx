c9b029e61e48c0de93a1b95dc9ea4d495e0f7dc7
pavel <pavel.sergutin@cronix.ms>
2018-07-23 18:12:49 +0300

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // libs
 import * as React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 // styles
 import './styles/index.scss';
 // pages
@@ -17,6 +17,7 @@ const Routing = () => (
   <Switch>
     <Route exact={true} path='/' component={Home}/>
     <Route path='/dashboard' component={Dashboard}/>
+    <Redirect to='/' />
   </Switch>
 );
 
@@ -34,21 +35,40 @@ class App extends React.Component<any, IState> {
     // this.showNav = false;
     console.log('App this', this);
   }
-  public render() {
+
+  public toggleSidebar = () => {
+    console.warn('toggleSidebar', this)
+    this.setState({
+      showSidebar: !this.state.showSidebar
+    });
+  };
+
+  public render () {
     return (
       <div className='app-layout'>
-        <aside className={`app-layout__sidebar ${this.state.showSidebar ? 'show-navbar' : ''}`}>
+        {/*<aside className={`app-layout__sidebar ${this.state.showSidebar ? 'app-layout__show-sidebar' : ''}`}>*/}
+          {/*aside*/}
+          {/*<div onClick={this.toggleSidebar}>*/}
+            {/*close*/}
+          {/*</div>*/}
+        {/*</aside>*/}
+        <aside className={'app-layout__sidebar minimized'}>
           aside
         </aside>
         <main className='app-layout__page'>
-          <header className='app-layout__header'>header</header>
+          <header className='app-layout__header' onClick={this.toggleSidebar}>
+            header 111111111111111111111111111
+            11111111111111111111111111111
+            1111111111111111111111111111111
+            111111111111111111111111111111111
+          </header>
           <div className='app-layout__content'>
 
             {/*here should be routing*/}
             <Routing/>
 
           </div>
-          <footer className='app-layout__footer'>footer</footer>
+          {/*<footer className='app-layout__footer'>footer</footer>*/}
         </main>
       </div>
     );

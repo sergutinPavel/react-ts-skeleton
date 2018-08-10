ed3e9827f767ca5b4de93d77cf6066185d9bd017
pavel <pavel.sergutin@cronix.ms>
2018-08-10 17:29:51 +0300

    redux devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,6 +7,9 @@ if (process.env.NODE_ENV !== 'production') {
   // console.log('process', process, process.env);
 }
 
+// const enhancer = ((window as any).devToolsExtension && process.env.NODE_ENV !== 'production')
+//   ? (window as any).devToolsExtension()(createStore) : createStore;
+
 const store = createStore(reducer, applyMiddleware());
 
 export default store;
